b13fd21a48b182e0f9e9ac2259f2828a77213328
Anna <annas.wieclaw@gmail.com>
2019-05-30 13:34:16 +0100

    Refactor rendering play view from post into get PostRedirectGet and introduce sessions

--- app.rb
+++ app.rb
@@ -4,13 +4,21 @@ set :session_secret, 'super secret'
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session[:message_player1] = params[:player1]
+    session[:message_player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:message_player1]
+    @player2 = session[:message_player2]
     erb :play
   end
   # start the server if ruby file executed directly
